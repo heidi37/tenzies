dfd313578dadaae1e2f81203fa340562b0f00307
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-29 06:58:20 -0400

    app render styled with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+import Die from './Die'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
   return (
     <>
-      <main></main>
+      <main>
+        <div className="inner-main">
+        <h1>Tenzies</h1>
+        <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dice-container">
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+          <Die/>
+        </div>
+        <button>Roll</button>
+        </div>
+      </main>
     </>
   )
 }
---
+++ src/Die.jsx
@@ -0,0 +1,7 @@
+import './Die.css'
+
+export default function Die() {
+  return (
+    <div className="die"><h2>1</h2></div>
+  )
+}
