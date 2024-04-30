0b32e773a77c4a46ffab1ad4423e339c313b9380
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-04-30 16:06:02 -0400

    adds css dots to dice

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,7 +1,49 @@
-import './Die.css'
+import "./Die.css"
 
 export default function Die(props) {
   return (
-    <div onClick={props.holdDice} className={props.isHeld ? "die held" : "die"}><h2>{props.value}</h2></div>
+    <div onClick={props.holdDice} className={props.isHeld ? "die held" : "die"}>
+      {props.value === 1 && <div class="middle-middle"></div>}
+      {props.value === 2 && (
+        <>
+          <div class="top-left"></div>
+          <div class="bottom-right"></div>
+        </>
+      )}
+      {props.value === 3 && (
+        <>
+          <div class="top-left"></div>
+          <div class="middle-middle"></div>
+          <div class="bottom-right"></div>
+        </>
+      )}
+      {props.value === 4 && (
+        <>
+          <div class="top-left"></div>
+          <div class="top-right"></div>
+          <div class="bottom-left"></div>
+          <div class="bottom-right"></div>
+        </>
+      )}
+      {props.value === 5 && (
+        <>
+          <div class="top-left"></div>
+          <div class="top-right"></div>
+          <div class="middle-middle"></div>
+          <div class="bottom-left"></div>
+          <div class="bottom-right"></div>
+        </>
+      )}
+      {props.value === 6 && (
+        <>
+          <div class="top-left"></div>
+          <div class="top-middle"></div>
+          <div class="top-right"></div>
+          <div class="bottom-left"></div>
+          <div class="bottom-middle"></div>
+          <div class="bottom-right"></div>
+        </>
+      )}
+    </div>
   )
 }
